1aab6a8d60af962dc7a5ded5d86eb08333fd7425
aguestuser <aguestuser@torproject.org>
2022-03-25 12:08:33 -0400

    add scrolling display of spoken lines

--- app/src/main/java/com/example/doomscrollingforgodot/MainActivity.kt
+++ app/src/main/java/com/example/doomscrollingforgodot/MainActivity.kt
@@ -3,18 +3,18 @@ package com.example.doomscrollingforgodot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.doomscrollingforgodot.data.SpokenLinesRepository
+import com.example.doomscrollingforgodot.doomscroller.SpokenLinesAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lines = SpokenLinesRepository.getInstance(this).lines
-
-        findViewById<TextView>(R.id.welcome_text).also {
-            it.text = lines[0].line
+        val spokenLinesRepository = SpokenLinesRepository.getInstance(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.doomscroller_recycler).also {
+            it.adapter = SpokenLinesAdapter(spokenLinesRepository.lines)
         }
-
     }
 }
---
+++ app/src/main/java/com/example/doomscrollingforgodot/doomscroller/SpokenLinesAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.doomscrollingforgodot.doomscroller
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.doomscrollingforgodot.R
+import com.example.doomscrollingforgodot.data.SpokenLine
+
+class SpokenLinesAdapter(private val spokenLines: List<SpokenLine>):
+    RecyclerView.Adapter<SpokenLinesAdapter.SpokenLineViewHolder>() {
+
+    class SpokenLineViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val speaker: TextView = itemView.findViewById(R.id.speaker_text)
+        private val line: TextView = itemView.findViewById(R.id.line_text)
+
+        fun bind(spokenLine: SpokenLine) {
+            speaker.text = spokenLine.speaker
+            line.text = spokenLine.line
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpokenLineViewHolder {
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.spoken_line_item, parent, false)
+        return SpokenLineViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: SpokenLineViewHolder, position: Int) {
+        val spokenLine = spokenLines[position]
+        holder.bind(spokenLine)
+    }
+
+    override fun getItemCount(): Int = spokenLines.size
+}
